8fe7ed176c43030e94a90525674bf0b43b9519b6
lalo1234321 <edominguezc@toluca.tecnm.mx>
2022-01-14 20:38:25 -0600

    Defining the project structure

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('EXAA II'),
+      ),
+      body: Center(
+        child: Text('Hello world'),
+      ),
+    );
+  }
+}
