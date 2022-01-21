aee32c99f74dcf5192b2eef798132b3ef73f5089
Oswaldo-EC <81650272+Oswaldo-EC@users.noreply.github.com>
2022-01-20 20:35:53 -0600

    Diseño pantalla Inicio sesión
    Se modificó el archivo pubspec.yaml para agregar em assets dos imágenes para mostrar el logo, se añadió una carpeta llamada assets, dentro de ella otra llamada img, y dentro de esta las imágenes utilizadas

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,17 +1,122 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _loading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('EXAA II'),
-      ),
-      body: Center(
-        child: Text('Hello world'),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 60),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [
+                Colors.deepPurple.shade800,
+                Colors.deepPurple.shade300,
+              ]),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 80),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  "assets/img/Logo1.png",
+                  color: Colors.white,
+                ),
+                Image.asset(
+                  "assets/img/Logo2.png",
+                ),
+              ],
+            ),
+          ),
+          Transform.translate(
+            offset: Offset(0, -10),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  margin: const EdgeInsets.only(
+                      left: 20, right: 20, top: 200, bottom: 20),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 35, vertical: 20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Usuario:"),
+                        ),
+                        SizedBox(height: 13),
+                        TextFormField(
+                          decoration: InputDecoration(labelText: "Contraseña:"),
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 18),
+                        RaisedButton(
+                          color: Theme.of(context).primaryColor,
+                          padding: const EdgeInsets.symmetric(vertical: 17),
+                          textColor: Colors.white,
+                          onPressed: () => _login(context),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text("Iniciar sesión"),
+                              if (_loading)
+                                Container(
+                                  height: 20,
+                                  width: 20,
+                                  margin: const EdgeInsets.only(left: 20),
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text("¿No estás registrado?"),
+                            FlatButton(
+                              textColor: Theme.of(context).primaryColor,
+                              child: Text("Registrarse"),
+                              onPressed: () => _showRegister(context),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  void _login(BuildContext context) {
+    if (!_loading) {
+      setState(() {
+        _loading = true;
+      });
+    }
+  }
+
+  void _showRegister(BuildContext context) {}
 }
