b7254b029c656f48a776d654640fd56c767c7f32
Brandon Rdgz <brandon.rdgz@gmail.com>
2022-01-28 19:16:31 -0600

    Add navigation to About Page from About button on Initial Page

--- lib/pages/initial_page.dart
+++ lib/pages/initial_page.dart
@@ -1,3 +1,4 @@
+import 'package:exaa_2/pages/about_page.dart';
 import 'package:exaa_2/services/firebase/auth.dart';
 import 'package:exaa_2/widgets/common_dialog.dart';
 import 'package:exaa_2/widgets/rounded_icon_text_button.dart';
@@ -52,7 +53,7 @@ class InitialPage extends StatelessWidget {
             text: 'Acerca de',
             textSize: 40,
             onPressed: () {
-              //TODO: Show page 'Acerca de'
+              Navigator.pushNamed(context, AboutPage.id);
             },
           ),
           RoundedIconTextButton(
