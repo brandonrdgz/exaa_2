369af952e0ecb11ebf582f2e554278259db98e87
Brandon Rdgz <brandon.rdgz@gmail.com>
2022-01-28 19:13:22 -0600

    Add About Page design

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,3 +1,4 @@
+import 'package:exaa_2/pages/about_page.dart';
 import 'package:exaa_2/pages/dummy_pages.dart';
 import 'package:exaa_2/pages/initial_page.dart';
 import 'package:exaa_2/pages/learning_page.dart';
@@ -14,6 +15,7 @@ class Routes {
     'topicLearning': (BuildContext context) => TopicLearningPage(),
     InitialPage.id: (BuildContext context) => InitialPage(),
     'register': (BuildContext context) => RegisterPage(),
-    'dummy': (BuildContext context) => DummyPage()
+    'dummy': (BuildContext context) => DummyPage(),
+    AboutPage.id: (BuildContext context) => AboutPage(),
   };
 }
