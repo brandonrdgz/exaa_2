ff99497f47327e64a0983e19248152b4cd3c1823
Brandon Rdgz <brandon.rdgz@gmail.com>
2022-05-01 19:25:36 -0500

    Fix modules of exam with the correct ones

--- lib/pages/apply_exam_page.dart
+++ lib/pages/apply_exam_page.dart
@@ -1,98 +1,162 @@
 import 'package:exaa_2/pages/exam_page.dart';
+import 'package:exaa_2/services/db_provider.dart';
+import 'package:exaa_2/utils/constants.dart';
 import 'package:exaa_2/widgets/common_dialog.dart';
 import 'package:flutter/material.dart';
 
-class ApplyExamPage extends StatelessWidget {
+class ApplyExamPage extends StatefulWidget {
   static const String id = 'ApplyExamPage';
 
-  const ApplyExamPage({Key? key}) : super(key: key);
+  ApplyExamPage({Key? key}) : super(key: key);
+
+  @override
+  State<ApplyExamPage> createState() => _ApplyExamPageState();
+}
+
+class _ApplyExamPageState extends State<ApplyExamPage> {
+  Widget _examInfo = Container();
 
   @override
   Widget build(BuildContext context) {
+    _initExamInfo(context);
+
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: ListView(
-            children: [
-              Text(
-                'El examen que estás a punto de presentar se compone de los siguientes módulos y preguntas:',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              DataTable(
-                columns: [
-                  DataColumn(label: Text('Módulo')),
-                  DataColumn(label: Text('Preguntas')),
-                ],
-                rows: [
-                  DataRow(
-                    cells: [
-                      DataCell(Text('Comprensión lectora')),
-                      DataCell(Text('30')),
-                    ],
-                  ),
-                  DataRow(
-                    cells: [
-                      DataCell(Text('Redacción indirecta')),
-                      DataCell(Text('30')),
-                    ],
-                  ),
-                  DataRow(
-                    cells: [
-                      DataCell(Text('Pensamiento matemático')),
-                      DataCell(Text('30')),
-                    ],
-                  ),
-                  DataRow(
-                    cells: [
-                      DataCell(Text('Cálculo')),
-                      DataCell(Text('24')),
-                    ],
-                  ),
-                  DataRow(
-                    cells: [
-                      DataCell(Text('Física')),
-                      DataCell(Text('24')),
-                    ],
-                  ),
-                ]
-              ),
-              Text(
-                'Siendo un total de 138 preguntas.\n',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              ElevatedButton(
-                child: Text('Aplicar examen'),
-                onPressed: () {
-                  CommonDialog.dialog(
-                    context,
-                    content: Text('¿Está seguro de presentar este examen?'),
-                    actions: [
-                      TextButton(
-                        child: const Text('Si'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                          Navigator.pushNamed(context, ExamPage.id);
-                        }
-                      ),
-                      TextButton(
-                        child: const Text('No'),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ]
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
+        body: _examInfo,
       ),
+    );
+  }
+
+  Future<bool> _dbHasNotQuestions() async {
+    List examModules = await DBProvider.db.getExamModules();
+
+    return examModules.isEmpty;
+  }
+
+  void _initExamInfo(BuildContext context) async {
+    if (await _dbHasNotQuestions()) {
+      CommonDialog.info(
+        context,
+        const Text('Info'),
+        const Text('Se necesita cargar información adicional para poder aplicar un examen'),
+        onAccept: () { 
+          Navigator.pop(context);
+          CommonDialog.waitDialog(
+            context,
+            content: const Text('Cargando información'),
+            future: DBProvider.db.insertQuestionsAndAnswers(),
+            onSuccess: (result) {
+              CommonDialog.info(
+                context,
+                const Text('Éxito'),
+                const Text('La información ha sido cargada correctamente'),
+                onAccept: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+              );
+            },
+            onError: (error) {
+              CommonDialog.error(context, error);
+            },
+          );
+        },
+      );
+    }
+    else {
+      setState(() {
+        _examInfo = _buildExamInfo(context);
+      });
+    }
+  }
+
+  Padding _buildExamInfo(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          const Text(
+            'El examen que estás a punto de presentar se compone de los siguientes módulos y preguntas:',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          _buildDataTable(),
+          const Text(
+            'Siendo un total de ${Constants.totalQuestionsOfExam} preguntas.\n',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          ElevatedButton(
+            child: const Text('Aplicar examen'),
+            onPressed: () {
+              _showConfirmDialog();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  DataTable _buildDataTable() {
+    return DataTable(
+      columns: const [
+        DataColumn(label: Text('Módulo')),
+        DataColumn(label: Center(child: Text('Preguntas'))),
+      ],
+      rows: const [
+        DataRow(
+          cells: [
+            DataCell(Text('Comprensión lectora')),
+            DataCell(Text('30')),
+          ],
+        ),
+        DataRow(
+          cells: [
+            DataCell(Text('Estructura de la lengua')),
+            DataCell(Center(child: Text('30'))),
+          ],
+        ),
+        DataRow(
+          cells: [
+            DataCell(Text('Pensamiento analítico')),
+            DataCell(Center(child: Text('30'))),
+          ],
+        ),
+        DataRow(
+          cells: [
+            DataCell(Text('Pensamiento matemático')),
+            DataCell(Center(child: Text('30'))),
+          ],
+        ),
+        DataRow(
+          cells: [
+            DataCell(Text('Cálculo')),
+            DataCell(Center(child: Text('24'))),
+          ],
+        ),
+        DataRow(
+          cells: [
+            DataCell(Text('Física')),
+            DataCell(Center(child: Text('24'))),
+          ],
+        ),
+      ]
+    );
+  }
+
+  void _showConfirmDialog() {
+    CommonDialog.confirm(
+      context,
+      const Text('¿Está seguro de presentar este examen?'),
+      onConfirm: () {
+        Navigator.pop(context);
+        Navigator.pushNamed(context, ExamPage.id);
+      },
+      onCancel: () {
+        Navigator.pop(context);
+      },
     );
   }
 }
